bed038eca4f3a78aeb6cdd4a07062ee4c1fbf1bc
Mohammad Shahanwaz <57594421+mshahanwaz@users.noreply.github.com>
2020-11-12 17:44:40 +0530

    Major Update

--- src/App.js
+++ src/App.js
@@ -1,51 +1,73 @@
-import React, { useEffect, useState } from 'react';
-import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
-import './App.css';
-import Todo from './Todo';
+import React, { useEffect, useState } from "react";
+import { Card, makeStyles, TextField } from "@material-ui/core";
+import "./App.css";
+import Todo from "./Todo";
 import db from "./firebase";
-import firebase from 'firebase';
+import firebase from "firebase";
 
 function App() {
   const [todos, setTodos] = useState([]);
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
 
-  // when the app loads, we need to listen to the database and getch new todos as they get added or remove
   useEffect(() => {
-    // this code here fires when the app.js loads
-    db.collection('todos').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
-      setTodos(snapshot.docs.map(doc => ({id: doc.id, todo: doc.data().todo})));
-    })
+    db.collection("todos")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setTodos(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            todo: doc.data().todo,
+          }))
+        );
+      });
   }, []);
+
   const addTodo = (event) => {
-    // This will add todo into todos list when hit enter
     event.preventDefault();
-    db.collection('todos').add({
-      todo: input,
-      timestamp: firebase.firestore.FieldValue.serverTimestamp()
-    });
-    setInput('');
-  }
+    if (input !== "") {
+      db.collection("todos").add({
+        todo: input,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      });
+    }
+    setInput("");
+  };
+
+  const useStyles = makeStyles(() => ({
+    card: {
+      backgroundColor: "rgb(250, 250, 246)",
+    },
+  }));
+
+  const classes = useStyles();
 
   return (
-    <div className="App">
-      <h1>My Do Todo App</h1>
-      <form>
-        <FormControl>
-          <InputLabel>Write a todo</InputLabel>
-          <Input value={input} onChange={event => setInput(event.target.value)}/>
-        </FormControl>
-        <Button disabled={!input} type='submit' onClick={addTodo} variant="contained" color="primary">
-        Add To-do
-        </Button>
-        {/* <button type='submit' onClick={addTodo}></button> */}
-      </form>
-      <ul>
-        {todos.map(todo => (
-          <Todo todo={todo}/>
-          // <li>{todo}</li>
+    <Card className={`app max-width ${classes.card}`}>
+      <div className="app__header">
+        <div className="app__title">
+          <p>T</p>
+          <p>O</p>
+          <p>D</p>
+          <p>O</p>
+        </div>
+        <form onSubmit={addTodo}>
+          <TextField
+            className={classes.margin}
+            id="outlined-secondary"
+            label="Post Here"
+            variant="outlined"
+            value={input}
+            fullWidth
+            onChange={(event) => setInput(event.target.value)}
+          />
+        </form>
+      </div>
+      <div className="app__todos">
+        {todos.map((todo) => (
+          <Todo todo={todo} className="app__todo" />
         ))}
-      </ul>
-    </div>
+      </div>
+    </Card>
   );
 }
 
